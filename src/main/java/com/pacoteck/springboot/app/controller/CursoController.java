b67377265790deb687a871da0cfe42fd069ff069
alvaro <bernabeyalvaro@gmail.com>
2022-04-01 16:51:50 +0200

    servicio y controlador de curso

--- src/main/java/com/pacoteck/springboot/app/controller/CursoController.java
+++ src/main/java/com/pacoteck/springboot/app/controller/CursoController.java
@@ -1,5 +1,44 @@
 package com.pacoteck.springboot.app.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.pacoteck.springboot.app.entity.Alumno;
+import com.pacoteck.springboot.app.entity.Curso;
+import com.pacoteck.springboot.app.service.CursoService;
+
+@RestController
+@RequestMapping(value = "/curso")
 public class CursoController {
 
+	@Autowired
+	public CursoService cs;
+	
+	
+	//trae todos los cursos de un profesor
+	@GetMapping(path = "/profesor{profesor}")
+	public List<Curso> cursosProfesor(@PathVariable("profesor") Long profesor){
+		return cs.cursosbyProfesor(profesor);
+	}
+	
+	@PostMapping(value = "/create")
+	public void create(@RequestBody Curso curso) {
+		cs.create(curso);
+	}
+	
+	//trae todos los alumnos de un curso
+	@GetMapping(path = "/alumno{curso}")
+	public List<Alumno> alumnosCurso(@PathVariable("curso") Long curso) {
+		return cs.alumnosbyProfesor(curso);
+	}
+	
+	
 }
--- src/main/java/com/pacoteck/springboot/app/service/CursoService.java
+++ src/main/java/com/pacoteck/springboot/app/service/CursoService.java
@@ -1,5 +1,100 @@
 package com.pacoteck.springboot.app.service;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.pacoteck.springboot.app.entity.Alumno;
+import com.pacoteck.springboot.app.entity.Curso;
+import com.pacoteck.springboot.app.repository.AlumnoRepository;
+import com.pacoteck.springboot.app.repository.CursoRepository;
+
+@Service
 public class CursoService {
 
+	@Autowired
+	public CursoRepository cr;
+	@Autowired
+	public AlumnoRepository ar;
+	
+	public void create(Curso curso) {
+		cr.save(curso);
+	}
+	
+	public void deleteById(Long curso) {
+		cr.deleteById(curso);
+	}
+	
+	public List<Curso> cursosbyProfesor(Long profesor){
+		List<Curso> cursos = cr.findByProfesor(profesor);
+		for (Curso curso : cursos) {
+			curso.setAlumnos(null);
+		}
+		return cursos;
+	}
+	
+	public List<Alumno> alumnosbyProfesor(Long curso){
+		List<Curso> cursos = cr.findAll();
+		List<Alumno> alumnos = null;
+		for (Curso curso2 : cursos) {
+			if(curso2.getId()  == curso) {
+				alumnos = curso2.getAlumnos();
+				for (Alumno alumno : alumnos) {
+					alumno.setInsigneas(null);
+					alumno.setTareas(null);
+				}
+				return alumnos;
+			}
+		}
+		return alumnos;
+	}
+	
+	public void addAlumno(Long alumno, Long curso) {
+		List<Curso> cursos = cr.findAll();
+		List<Alumno> alumnos = ar.findAll();
+		Alumno alumnoAux = null;
+		Curso cursoAux = null;
+		for (Alumno alumno2 : alumnos) {
+			if(alumno2.getId() == alumno) {
+				alumnoAux = alumno2;
+				break;
+			}
+			alumnos = null;
+		}
+		for (Curso curso2 : cursos) {
+			if(curso2.getId() == curso) {
+				cursoAux = curso2;
+				alumnos = curso2.getAlumnos();
+				break;
+			}
+		}
+		alumnos.add(alumnoAux);
+		cr.save(cursoAux);
+	}
+	
+	public void deleteAlumno(Long alumno, Long curso){
+		List<Curso> cursos = cr.findAll();
+		List<Alumno> alumnos = ar.findAll();
+		Alumno alumnoAux = null;
+		Curso cursoAux = null;
+		for (Alumno alumno2 : alumnos) {
+			if(alumno2.getId() == alumno) {
+				alumnoAux = alumno2;
+				break;
+			}
+			alumnos = null;
+		}
+		for (Curso curso2 : cursos) {
+			if(curso2.getId() == curso) {
+				cursoAux = curso2;
+				alumnos = curso2.getAlumnos();
+				break;
+			}
+		}
+		alumnos.remove(alumnoAux);
+		cursoAux.setAlumnos(alumnos);
+		cr.save(cursoAux);
+	}
 }
